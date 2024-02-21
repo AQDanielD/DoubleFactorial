192103f3bdbf21079718b68ce6c441466f3c51cb
Daniel Davies <AQ232596@aquinas.ac.uk>
2024-02-21 22:51:53 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleFactorial
+{
+    internal class Program
+    {
+        static public void Print(List<int> list, int i)
+        {
+            if (i < list.Count - 1)
+            {
+                Console.Write($"{list[i]}*");
+            }
+            else
+            {
+                Console.Write($"{list[i]}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Input: ");
+
+            int num = int.Parse(Console.ReadLine());
+
+            List<int> list = new List<int>();
+            list.Add(num);
+
+            int j = num;
+            while (j > 1)
+            {
+                j -= 2;
+                if (j > 0)
+                {
+                    list.Add(j);
+                }
+            }
+
+            int total = 1; // Initialize total to 1
+
+            Console.Write($"Output: {num}!!   =   ");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                total *= list[i];
+                Print(list, i);
+            }
+
+            Console.Write($"    =   {total}");
+            Console.ReadLine();
+        }
+    }
+}
+
